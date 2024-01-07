0d1d3c0152823324b64fde9da26b2d8002ce4598
Izzat Z. <izzat.zainir11@gmail.com>
2024-01-07 23:35:17 +0800

    Install some QOL stuff

---
+++ lua/izz/plugins/nvim-surround.lua
@@ -0,0 +1,22 @@
+return {
+	{
+		"kylechui/nvim-surround",
+		version = "*",
+		event = "VeryLazy",
+		opts = {
+			keymaps = {
+				insert = "<C-s>s",
+				insert_line = "<C-s>S",
+				normal = "<leader>Ss",
+				normal_cur = "<leader>SS",
+				normal_line = "<leader>Sa",
+				normal_cur_line = "<leader>SA",
+				visual = "<leader>Ss",
+				visual_line = "<leader>Sl",
+				delete = "<leader>Sd",
+				change = "<leader>Sc",
+				change_line = "<leader>SC",
+			},
+		},
+	},
+}
